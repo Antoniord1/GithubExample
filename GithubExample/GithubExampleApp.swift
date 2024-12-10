a956c358562b2a3f95b4d94a7b52d967547a4b89
Allison Dawson <aadawson!@hotmail.com>
2024-12-10 09:50:31 -0500

    Initial Commit

---
+++ GithubExample/GithubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExampleApp.swift
+//  GithubExample
+//
+//  Created by Allison Dawson on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubExampleTests/GithubExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExampleTests.swift
+//  GithubExampleTests
+//
+//  Created by Allison Dawson on 12/10/24.
+//
+
+import Testing
+@testable import GithubExample
+
+struct GithubExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
